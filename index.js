a3cb032116216a5418ecb555e0feb00f6ac71c23
Kim0Jae <youngjae1047@naver.com>
2017-07-08 22:11:10 +0900

    Test Server

--- index.js
+++ index.js
@@ -22,5 +22,5 @@ app.use(function (req, res, next) {
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!');
+    console.log('app listening on port 3000!');
 });
